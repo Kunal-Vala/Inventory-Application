cadbfab826d4fe617bf6b6df5422ee4aa4f7ae80
Kunal-Vala <kunalvala2005@gmail.com>
2025-10-29 18:17:26 +0530

    add-query : readItemById

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require('./pool')
 
 async function readAllItems() {
-    const { rows } = await pool.query(`SELECT 
+  const { rows } = await pool.query(`SELECT 
   item.id AS item_id,
   item.name AS item_name,
   item.description,
@@ -11,29 +11,42 @@ async function readAllItems() {
 FROM item
 JOIN category ON item.category_id = category.id;
 `)
-    return rows
+  return rows
 }
 
 async function insertItem(item) {
-    const query = `
+  const query = `
     INSERT INTO item (name, description, category_id, qty)
     VALUES ($1, $2, $3, $4)
   `;
-    const values = [item.name, item.description, item.category_id, item.qty];
+  const values = [item.name, item.description, item.category_id, item.qty];
 
+  try {
     await pool.query(query, values);
+    console.log('Item inserted successfully');
+  } catch (err) {
+    console.error('INSERTION FAILED:', err);
+  }
 }
 
 
 async function readCategories() {
   const query = 'SELECT * FROM category'
-  const {rows} = await pool.query(query)
-  
+  const { rows } = await pool.query(query)
+
   return rows
 }
 
+
+async function readItemById(id) {
+  const result = await pool.query("SELECT * FROM item WHERE id = ($1)", [id])
+
+  return result.rows[0]
+}
+
 module.exports = {
-    readAllItems,
-    insertItem,
-    readCategories
+  readAllItems,
+  insertItem,
+  readCategories,
+  readItemById
 } 
