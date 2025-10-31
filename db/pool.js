a35cd87663ec763bee71999b2a40d1e77a8ebe36
Kunal-Vala <kunalvala2005@gmail.com>
2025-10-31 14:16:50 +0530

    fix : pool module

--- db/pool.js
+++ db/pool.js
@@ -12,13 +12,11 @@ require('dotenv').config();
 
 
 
-import pkg from 'pg';
-
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-export default pool;
+module.exports = pool;
